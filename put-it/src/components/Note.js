fb58dc733ec9c3c639df85f6af7012d5c9f89f1f
Anton Nordin <antno521@student.liu.se>
2024-11-29 16:08:35 +0100

    Fixed boarder and added Note component

---
+++ put-it/src/components/Note.js
@@ -0,0 +1,7 @@
+export default function Note() {
+  return (
+    <div className="size-40 bg-yellow-200">
+      hej
+    </div>
+  )
+}
--- put-it/src/components/Whiteboard.js
+++ put-it/src/components/Whiteboard.js
@@ -1,6 +1,9 @@
+import Note from "./Note"
+
 export default function Whiteboard() {
   return (
-    <div className="w-screen h-screen border-4 border-blue-500">
+    <div className="w-screen h-[calc(100vh-42px)] border-4 border-blue-500">
+      <Note />
     </div>
   )
 }
