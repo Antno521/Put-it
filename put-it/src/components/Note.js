0623834d01b18d7b6ed404b1e4fe14e67dcd90e4
Anton Nordin <antno521@student.liu.se>
2024-12-02 13:00:02 +0100

    Added borde and onclick

--- put-it/src/components/Note.js
+++ put-it/src/components/Note.js
@@ -1,6 +1,20 @@
+import { useState, useEffect } from "react";
+
 export default function Note() {
+  const [xCoord, setXCoord] = useState(40);
+  const [yCoord, setYCoord] = useState(500);
+  const [borderColor, setBorderColor] = useState('border-yellow-200')
+
+  const handleNoteClick = () => {
+    setBorderColor(prev => prev === 'border-yellow-200' ? 'border-blue-500' : 'border-yellow-200');
+  }
+
+  useEffect(() => {
+    console.log("hejsvejs");
+  })
+
   return (
-    <div className="size-40 bg-yellow-200">
+    <div className={`size-40 bg-yellow-200 border-2 relative ${borderColor}`} style={{top: `${xCoord}px`, left: `${yCoord}px`}} onClick={handleNoteClick}>
       hej
     </div>
   )
