3c8b54a4c636e8c75c73a9ac00271c050e379e91
Anton Nordin <antno521@student.liu.se>
2024-12-02 13:00:55 +0100

    Added mouse event and on click

--- put-it/src/components/Whiteboard.js
+++ put-it/src/components/Whiteboard.js
@@ -1,8 +1,16 @@
 import Note from "./Note"
 
 export default function Whiteboard() {
+  function handleMouseMove(event){
+    console.log('Mouse position: ', event.clientX, event.clientY);
+  }
+
+  const handleWhiteboardClick = () => {
+    console.log('rara')
+  }
+  
   return (
-    <div className="w-screen h-[calc(100vh-42px)] border-4 border-blue-500">
+    <div className="w-screen h-[calc(100vh-42px)] border-4 border-blue-500" onMouseMove={handleMouseMove} onClick={handleWhiteboardClick}>
       <Note />
     </div>
   )
